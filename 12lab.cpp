3be3b7bf96641181cbaadc7e5469bf7a2043a8c6
Kirill Prokhorenko <kirkin.kp@gmail.com>
2020-02-27 00:40:08 +0300

    [create repository]

---
+++ 12lab.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+struct x
+{
+    int *p;
+};
+
+int main()
+{
+    x *h[50];
+    for (int i = 0; i < 50; i++)
+    {
+        h[i] = new x;
+        h[i]->p = new int;
+     //   h[i]->p = 1;
+    }
+}
