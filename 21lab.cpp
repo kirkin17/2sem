782d295303966da689cde157f12a3169914715e3
Kirill Prokhorenko <kirkin.kp@gmail.com>
2020-02-29 00:39:21 +0300

    lab1 final

--- 21lab.cpp
+++ 21lab.cpp
@@ -1,23 +1,25 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
-
 struct x
 {
-    int *p;
+	int* p;
 };
-
 int main()
 {
-    //1 задание
-    x* h[50];
-    for (int i = 0; i < 50; i++)
-    {
-        h[i] = new x;
-        h[i]->p = new int;
-        *(h[i]->p) = 1;
-
-    }
-
-    //2 задание
-    x* 
+	x* h[50];	
+	for (int i = 0; i < 50; i++)
+	{
+		h[i] = new x;		
+		h[i]->p = new int;
+		*(h[i]->p) = 1;
+	}
+	x* g = new x[100];
+	for (int i = 0; i < 100; i++)
+	{
+		g[i].p = new int[20];
+		for (int j = 0; j < 20; j++)
+		{
+			g[i].p[j] = 1;
+		}
+	}
 }
