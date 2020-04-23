a5cde7738e48ba0f1dfbb274551fd2038b415ef2
Kirill Prokhorenko <kirkin.kp@gmail.com>
2020-04-24 01:29:14 +0300

    123

--- MUL_NN_N.cpp
+++ MUL_NN_N.cpp
@@ -1,4 +1,25 @@
+#include <iostream>
+using namespace std;
 //Прохоренко Кирилл 9371
 
+void MUL_NN_N(int a[], int na, int b[], int nb)
+{
+    if (na > nb)
+    {
+        for (int i = na; i >= 0; --i)
+        {
 
+        }
+    }
+}
 
+void mani()
+{
+    using namespace std;
+    int *na, *nb;
+    int* a, *b;
+    na = new int;
+    nb = new int;
+    cin >> na;
+    
+}
