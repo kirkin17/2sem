978cc8dc62884290dc97397309abf9027440d0b9
Kirill Prokhorenko <kirkin.kp@gmail.com>
2020-09-14 02:40:39 +0300

    1234 6(в процессе)

--- kursovaya.cpp
+++ kursovaya.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 
-
 struct stud
 {
     string name; // имя
@@ -18,50 +17,38 @@ struct stud
 struct group
 {
     stud *begin = NULL; //указатель на начало списка студентов группы
-    stud *current = NULL;
     group* nextgr = NULL; //следующая группа
     int count = 0; //количество студентов в группе
     int num = 0; //номер группы
 };
 
+group *beging = NULL; //сразу создаем указатель для хранения начала групп, этот адрес нам нужен во всей программе
+
 string inputname()
 {
     string name;
-    cin.ignore(256, '\n');
-    getline(cin, name);
+    std::cin.ignore(256, '\n');
+    getline(std::cin, name);
     return name;
 }
 
-
-group* inputstudent(group *begingr)
+void inputstudent(stud *p)
 {
-    using namespace std;
-    stud *p = new stud;
-
-    system("clear");
-    cout << "Введите ФИО студента: ";
-    p->name = inputname();
-    cout << "Введите номер группы: ";
-    int numgroup;
-    cin >> numgroup; //вводят номер группы, далее проверяем есть ли такая группа, если да, то добавляем в нее, иначе создаем новую
-    p->group = numgroup;
-
-    
-    group *gr = begingr;
-    if (gr == NULL) 
+    group *gr = beging;
+    if (gr == NULL)
     {
-        begingr = new group;
-        gr = begingr;
-        gr->num = numgroup;
+        beging = new group;
+        gr = beging;
+        gr->num = p->group;
         gr->begin = p;
     }
     else
     {
-        while (gr->nextgr != NULL && gr->num != numgroup)
+        while (gr->nextgr != NULL && gr->num != p->group)
         {
             gr = gr->nextgr;
         }
-        if (gr->num == numgroup)
+        if (gr->num == p->group)
         {
             stud *k = gr->begin;
             while(k->next != NULL)
@@ -72,7 +59,7 @@ group* inputstudent(group *begingr)
         }
         else
         {
-            gr = begingr;
+            gr = beging;
             while(gr->nextgr != NULL)
             {
                 gr = gr->nextgr;
@@ -80,120 +67,206 @@ group* inputstudent(group *begingr)
             gr->nextgr = new group;
             gr = gr->nextgr;
             gr->begin = p;
-            gr->num = numgroup;
-        }
-            
-            
+            gr->num = p->group;
+        }  
     }
-    
-    
-
-    cout << "Введите оценки: \n";
-    int grades[5];
-    for(int i = 0; i < 5; i++)
-    {
-        printf("[%d]. ", i + 1);
-        cin.ignore(256, '\n');
-        cin >> grades[i];
-        p->grades[i] = grades[i];
-    }
-    cout << "Введите размер стипендии: ";
-    int stipend;
-    cin.ignore(256, '\n');
-    cin >> stipend;
-    p->stipend = stipend;
-    return begingr;
 }
 
-
-/*
-void newstudentkeyboard(group *current)
+group inputkeyboard()
 {
-    current->current = new stud;
-    if (current->begin == NULL)
-    {
-        current->begin = current->current;
-    }
+    stud *p = new stud;
 
     system("clear");
-    cout << "Введите ФИО студента: ";
-    current->current->name = inputname();
-    cout << "Введите номер группы: ";
-    int group;
-    cin >> group;
-    current->current->group = group;
-    current->num = group;
-    cout << "Введите оценки: ";
+    std::cout << "Введите ФИО студента: ";
+    p->name = inputname();
+    std::cout << "Введите номер группы: ";
+    int numgroup;
+    std::cin >> numgroup; //вводят номер группы, далее проверяем есть ли такая группа, если да, то добавляем в нее, иначе создаем новую
+    p->group = numgroup;
+
+
+    group *gr = beging;
+    
+
+    std::cout << "Введите оценки: \n";
     int grades[5];
     for(int i = 0; i < 5; i++)
     {
         printf("[%d]. ", i + 1);
-        cin.ignore(256, '\n');
-        cin >> grades[i];
-        current->current->grades[i] = grades[i];
+        std::cin.ignore(256, '\n');
+        std::cin >> grades[i];
+        p->grades[i] = grades[i];
     }
-    cout << "Введите размер стипендии: ";
+    std::cout << "Введите размер стипендии: ";
     int stipend;
-    cin.ignore(256, '\n');
-    cin >> stipend;
-    current->current->stipend = stipend;
+    std::cin.ignore(256, '\n');
+    std::cin >> stipend;
+    p->stipend = stipend;
+    inputstudent(p);
 }
-*/
+
+void outputkeyboardfordelete(group *p)
+{
+    system("clear");
+    unsigned int key = 1;
+    while (p != NULL)
+    {
+        std::cout << "Группа: " << p->num << endl;
+        stud *k;
+        k = p->begin;
+        while(k != NULL)
+        {
+            std::cout << "     " << key << ". ";
+            std::cout << k->name << endl;
+            k = k->next;
+            key++;
+        }
+        p = p->nextgr;        
+    }
+    std::cout << "\n\nДля продолжения нажмите enter...\n";
+    //std::cin.ignore(256, '\n');
+    getchar();
+    //std::cin.ignore(256, '\n');
+}
+
 
 void outputkeyboard(group *p)
 {
     system("clear");
     while (p != NULL)
     {
-        cout << "Группа: " << p->num << endl;
+        std::cout << "Группа: " << p->num << endl;
         stud *k;
         k = p->begin;
         while(k != NULL)
         {
-            cout << "\tФИО:" << k->name << endl;
-            //cout << "\tГруппа: " << k->group << endl;
-            cout << "\tСтипендия: " << k->stipend << endl;
-            cout << "\tОценки:\n";
+            std::cout << "\tФИО:" << k->name << endl;
+            //std::cout << "\tГруппа: " << k->group << endl;
+            std::cout << "\tСтипендия: " << k->stipend << endl;
+            std::cout << "\tОценки:\n";
             for (int i = 0; i < 5; i++)
             {
-                cout << "\t\t" << i + 1 << ". " << k->grades[i] << endl;
+                std::cout << "\t\t" << i + 1 << ". " << k->grades[i] << endl;
             }
+            cout << endl;
             k = k->next;
         }
         p = p->nextgr;        
     }
-    cout << "\n\nДля продолжения нажмите enter...\n";
-    cin.ignore(256, '\n');
+    std::cout << "\n\nДля продолжения нажмите enter...\n";
+    std::cin.ignore(256, '\n');
     getchar();
+}
+
+void inputfile()
+{
+    ifstream file("in.txt");
+    if (file.is_open())
+	{
+		while (!file.eof())
+		{
+			stud* st = new stud;
+			getline(file, st->name);
+			if (st->name == "") //если прочитана пустая строка не создавать студента
+			{
+			    delete st;
+				break;
+			}
+			file >> st->group;
+
+
+			for (int i = 0; i < 5; i++) file >> st->grades[i];
+			file >> st->stipend;
+			inputstudent(st);
+			file.get();//переходим на следующую строку
+		}
+		file.close(); 
+	}
+}
+
+void outputfile() //вывод в файл
+{
+	ofstream file("out.txt"); //открытие файла на запись
+    group *gr = beging;
+    while(gr != NULL)
+    {
+        file << "Группа: " << gr->num << endl;
+	    stud* st = gr->begin;
+	    while (st != NULL)
+		{
+			file << "\tФИО:" << st->name << endl;
+            file << "\tСтипендия: " << st->stipend << endl;
+            file << "\tОценки:\n";
+            for (int i = 0; i < 5; i++)
+            {
+                file << "\t\t" << i + 1 << ". " << st->grades[i] << endl;
+            }
+            file << endl;
+			st = st->next;
+		}
+        gr = gr->nextgr;
+    }
+	file.close();
+}
+
+void changeinfo(group *gr)
+{
+
+}
+
+void deletestudent(group *gr)
+{
+    system("clear");
+    cout << "Чтобы удалить студента, будет выведен список студентов. Запомните номер студента и затем введите его!\n";
+    cout << "Или введите полностью ФИО\n";
+    std::cin.ignore(256, '\n');
+    std::getchar();
+    outputkeyboardfordelete(beging);
+
 }
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
     int menu;
-
-    group *begin = NULL; //сразу создаем указатели для создания хотя бы одной группы
     
     do
     {
         system("clear");
-        cout << "Меню" << endl << " 1. Ввести данные о студенте с клавиатуры" << endl;
-        cout << " 2. Вывести список студентов на экран" << endl;
-        cout << " 3. Ввести данные о студентах из файла (\"in.txt\")" << endl;
-        cout << " 4. Вывести список студентов в файл (\"out.txt\")" << endl;
-        cout << " 5. Изменить данные о студенте" << endl;
-        cout << " 6. Удалить студента" << endl;
-        cout << " 7. Удалить группу" << endl;
-        cout << " 8. Вывести студентов, сгруппированных по размеру стипендии(ИДЗ)" << endl;
-        cout << " 0. Выход" << endl;
-        cin >> menu;
+        std::cout << "Меню" << endl << " 1. Ввести данные о студенте с клавиатуры" << endl;
+        std::cout << " 2. Вывести список студентов на экран" << endl;
+        std::cout << " 3. Ввести данные о студентах из файла (\"in.txt\")" << endl;
+        std::cout << " 4. Вывести список студентов в файл (\"out.txt\")" << endl;
+        std::cout << " 5. Изменить данные о студенте" << endl;
+        std::cout << " 6. Удалить студента" << endl;
+        std::cout << " 7. Удалить группу" << endl;
+        std::cout << " 8. Вывести студентов, сгруппированных по размеру стипендии(ИДЗ)" << endl;
+        std::cout << " 0. Выход" << endl << endl;
+        std::cout << "Выберите действие: ";
+        std::cin >> menu;
         if (menu == 1)
         {
-            begin = inputstudent(begin);
+            inputkeyboard();
         }
         if (menu == 2)
         {
-            outputkeyboard(begin);
+            outputkeyboard(beging);
+        }
+        if (menu == 3)
+        {
+            inputfile();
+        }
+        if (menu == 4)
+        {
+            outputfile();
+        }
+        if (menu == 5)
+        {
+            changeinfo(beging);
+        }
+        if (menu == 6)
+        {
+            deletestudent(beging);
         }
     }
     while (menu != 0);
